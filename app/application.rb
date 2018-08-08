936461c664bd124fe987866234b8ee0b47e7cc94
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-08 07:11:38 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,11 @@
+lass Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
+      if item
+  end
+end
